d468ccd46e00870f45182c6fe1ae2a99f1cfdec9
Yaroslav82 <yargay230607@gmail.com>
2023-09-17 18:53:32 +0300

    Add all args constructor and toString method

--- src/main/java/org/example/app/entities/Employee.java
+++ src/main/java/org/example/app/entities/Employee.java
@@ -1,14 +1,29 @@
 package org.example.app.entities;
 
+import java.sql.Date;
+
 public class Employee {
 
     private int id;
     private String lastName;
     private String firstName;
-    private String birthDate;
+    private Date birthDate;
     private int positionId;
     private String phone;
     private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String lastName, String firstName, Date birthDate, int positionId, String phone, double salary) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+        this.positionId = positionId;
+        this.phone = phone;
+        this.salary = salary;
+    }
 
     public int getId() {
         return id;
@@ -34,11 +49,11 @@ public class Employee {
         this.firstName = firstName;
     }
 
-    public String getBirthDate() {
+    public Date getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(String birthDate) {
+    public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
     }
 
@@ -64,5 +79,16 @@ public class Employee {
 
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id +
+                ", " + firstName +
+                ", " + lastName +
+                ", " + birthDate +
+                ", " + phone +
+                ", position id: " + positionId +
+                ", salary: " + salary;
     }
 }
