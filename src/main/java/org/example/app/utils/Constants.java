914320db7a091a809620f991c220aed6e9940215
Yaroslav82 <yargay230607@gmail.com>
2023-09-17 18:57:09 +0300

    Update Constants

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -15,6 +15,19 @@ public final class Constants {
     public final static String TABLE_POSITIONS = "positions";
     public final static String TABLE_EMPLOYEES = "employees";
 
+    public final static String PHONE_RGX = "[0-9]{3}[\\ ][0-9]{3}-[0-9]{4}";
+    public final static String WRONG_PHONE_MSG = "Wrong phone input.";
+
+    public final static String DATE_RGX = "\\d{4}-[01]\\d-[0-3]\\d";
+    public final static String WRONG_DATE_MSG = "Wrong date input.";
+
+    public final static String ID_RGX = "^[1-9]$";
+    public final static String WRONG_ID_MSG = "Wrong id input.";
+    public final static String UNKNOWN_ID_MSG = "Unknown position id.";
+
+    public final static String SALARY_RGX = "\\d+";
+    public final static String WRONG_SALARY_MSG = "Wrong salary input.";
+
     public final static String INCORRECT_VALUE_MSG = ">> Incorrect value! Try again...";
     public final static String APP_CLOSE_MSG = "\n>> App closed.";
     public final static String INPUT_REQ_MSG = "Input required.";
